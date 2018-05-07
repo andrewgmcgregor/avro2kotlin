8832f895127f06f72ccb555e1b6d3963ac478a7e
andrew <andrew.g.mcgregor@gmail.com>
2018-05-07 22:59:48 +1000

    refactor

--- src/main/java/main/SchemaUtils.java
+++ src/main/java/main/SchemaUtils.java
@@ -5,23 +5,32 @@ import org.apache.avro.compiler.idl.Idl;
 
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public class SchemaUtils {
-    public static Collection<Schema> getSchemasForAvdl(String resourcePath) {
+    public static Collection<Schema> getSchemasForAvdl(String resourcePath) throws Exception {
+        return getSchemaForAvsc(resourcePath, in -> tryOrThrow(() -> new Idl(in).ProtocolDeclaration().getTypes()));
+    }
+
+    public static Schema getSchemaForAvsc(String resourcePath) throws Exception {
+        return getSchemaForAvsc(resourcePath, in -> tryOrThrow(() -> new Schema.Parser().parse(in)));
+    }
+
+    public static <R> R getSchemaForAvsc(String resourcePath, Function<InputStream, R> f) throws Exception {
         ClassLoader classLoader = SchemaUtils.class.getClassLoader();
         try (InputStream in = classLoader.getResourceAsStream(resourcePath)) {
-            return new Idl(in).ProtocolDeclaration().getTypes();
+            return f.apply(in);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static Schema getSchemaForAvsc(String resourcePath) throws Exception {
-        ClassLoader classLoader = SchemaUtils.class.getClassLoader();
-        try (InputStream in = classLoader.getResourceAsStream(resourcePath)) {
-            return new Schema.Parser().parse(in);
+    private static <T> T tryOrThrow(Callable<T> value) {
+        try {
+            return value.call();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new UnsupportedOperationException(e);
         }
     }
 }
