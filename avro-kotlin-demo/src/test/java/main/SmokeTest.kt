9c8c7be2abadacdf9a98371161d3bfff16e1a437
andrew <andrew.g.mcgregor@gmail.com>
2018-05-11 17:20:25 +1000

    uncomment exampleperson test

--- avro-kotlin-demo/src/test/java/main/SmokeTest.kt
+++ avro-kotlin-demo/src/test/java/main/SmokeTest.kt
@@ -2,21 +2,20 @@ package main
 
 import demo.Example
 import demo.ExampleNesting
+import demo.ExamplePerson
 import org.junit.Test
 
 class SmokeTest {
 
     @Test
     fun smokeTest() {
-//    thereAndBackAgain(ExamplePerson(42, "user1"))
-//    thereAndBackAgain(ExamplePerson(99, null))
-////    thereAndBackAgain(ExamplePerson(null, "user2"))
+        thereAndBackAgain(ExamplePerson(42, "user1"))
+        thereAndBackAgain(ExamplePerson(99, null))
 
         var falseNesting = ExampleNesting(false)
         var trueNesting = ExampleNesting(true)
         thereAndBackAgain3(falseNesting)
         thereAndBackAgain3(trueNesting)
-//    thereAndBackAgain3(ExampleNesting(null))
 
         thereAndBackAgain2(Example(42, falseNesting, falseNesting, "user1"))
         thereAndBackAgain2(Example(99, trueNesting, trueNesting, null))
@@ -24,17 +23,17 @@ class SmokeTest {
 
     }
 
-//private fun thereAndBackAgain(originalAvroSpecificRecord: ExamplePerson) {
-//    println("originalAvroSpecificRecord = ${originalAvroSpecificRecord}")
-//
-//    val kotlinDataClass = demo.ExamplePersonKt.fromAvroSpecificRecord(originalAvroSpecificRecord)
-//    println("kotlinDataClass = ${kotlinDataClass}")
-//
-//    val toAvroSpecificRecord = kotlinDataClass.toAvroSpecificRecord()
-//    println("toAvroSpecificRecord = ${toAvroSpecificRecord}")
-//
-//    println()
-//}
+    private fun thereAndBackAgain(originalAvroSpecificRecord: ExamplePerson) {
+        println("originalAvroSpecificRecord = ${originalAvroSpecificRecord}")
+
+        val kotlinDataClass = demo.ExamplePersonKt.fromAvroSpecificRecord(originalAvroSpecificRecord)
+        println("kotlinDataClass = ${kotlinDataClass}")
+
+        val toAvroSpecificRecord = kotlinDataClass.toAvroSpecificRecord()
+        println("toAvroSpecificRecord = ${toAvroSpecificRecord}")
+
+        println()
+    }
 
     fun thereAndBackAgain2(example: Example) {
         println("originalAvroSpecificRecord = ${example}")
